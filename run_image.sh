9e91d63f67b7fc1b7da08ffc888ba031fe0a83d0
Stuart Buchanan <stuart13@gmail.com>
2024-06-13 22:26:01 +0100

    WS30 - add mount for separate OSM cache directory

--- run_image.sh
+++ run_image.sh
@@ -5,7 +5,9 @@ BASE_DIR="/home/flightgear"
 mkdir -p output
 docker run \
  --rm \
+ --mount "type=bind,source=`pwd`/cache,target=${BASE_DIR}/cache" \
  --mount "type=bind,source=`pwd`/data,target=${BASE_DIR}/data,readonly" \
  --mount "type=bind,source=`pwd`/output,target=${BASE_DIR}/output" \
  -it \
  flightgear/ws30-vpb-generator:v1.6 /bin/bash $1
+ 
